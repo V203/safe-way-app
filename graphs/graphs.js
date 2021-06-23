32253bf43e00cebed73832f150bd17f3e93e9cdb
v203 <vuyisamr203@gmail.com>
2021-06-23 16:41:44 +0200

    cdx

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -1,30 +1,97 @@
-const  bus = {
-    // locations:[ 'Cape Town', 'wood stock','Salt river','Koeberg','Maitland','Pinelands','Ndabeni',"Langa"],
-    
-    'Cape Town':0,
-    'wood stock':0,
-    'Salt river':0,
-    'Koeberg':0,
-    'Maitland':0,
-    'Pinelands':0,
-    'Ndabeni':0,
-    "Langa":0
+var disembarking_btn = document.querySelector(".getoff");
+var boarding_btn = document.querySelector(".geton")
+var add_btn = document.querySelector(".add_btn")
+var nextD_btn = document.querySelector(".nextd_btn")
+
+
+// const  bus = {
+
+
+//     // 'Cape Town':8,
+//     // 'wood stock':2,
+//     // 'Salt river':0,
+//     // 'Koeberg':0,
+//     // 'Maitland':0,
+//     // 'Pinelands':1,
+//     // 'Ndabeni':0,
+//     // "Langa":0
+// }
+
+var bus = [
+  { name: "Cape Town", passengers: 1 },
+  { name: "wood stock", passengers: 2 },
+  { name: "Saltriver", passengers: 3 },
+];
+var townName = []
+var temp = []
+
+var tempUpdated = [];
+
+for (let i = 0; i < bus.length; i++) {
+
+  temp.push(bus[i].passengers)
+  townName.push(bus[i].name)
+  // console.log(bus[key]);
+
 }
 
 
-const data = {
-  labels: Object.keys(bus) ,
+// disembarking_btn.addEventListener("click",function(){
+
+//    temp = Object.keys(bus)
+
+
+
+// })
+
+boarding_btn.addEventListener("click", function (e) {
+
+e.preventDefault()
+  var objIndex = bus.findIndex(function (elem) {
+    return elem.name == "Saltriver";
+
+  });
+  console.log(objIndex)
+
+  bus[objIndex].passengers = bus[objIndex].passengers += 1
+  // console.log(bus[objIndex])
+  // console.log(bus)
+
+  temp = []
+  // for (let i = 0; i < bus.length; i++) {
+
+  //   tempUpdated.push(bus[i].passengers)
+  //   console.log(bus[i].passengers);
+  // }
+
+  //alert("hghg")
+})
+
+// add_btn.addEventListener("click",function(){
+//  bus['wood stock']+=1
+
+// })
+
+
+// nextD_btn.addEventListener("click",function(){
+//     alert()
+
+// })
+
+var data = {
+  labels: townName,
   datasets: [{
     label: 'Bus No GA-4526',
     backgroundColor: 'rgb(255, 99, 132)',
     borderColor: 'rgb(255, 99, 244)',
-    data: Object.values(bus),
+    data: temp,
   }]
 };
 
 
-const config = {
-    type: 'line',
-    data,
-    options: {}
-  };
+var config = {
+  type: 'bar',
+  data
+};
+
+
--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,31 @@
-var disembarking_btn = document.querySelector(".getoff");
-var boarding_btn = document.querySelector(".geton")
-var add_btn = document.querySelector(".add_btn")
-var nextD_btn =  document.querySelector(".nextd_btn")
+// var disembarking_btn = document.querySelector(".getoff");
+// var boarding_btn = document.querySelector(".geton")
+// var add_btn = document.querySelector(".add_btn")
+// var nextD_btn =  document.querySelector(".nextd_btn")
 
 
-disembarking_btn.addEventListener("cilck",function(){
-alert("test get off");
-})
 
-boarding_btn.addEventListener("cilck",function(){
-    alert("test get on");
-})
 
-add_btn.addEventListener("click",function(){
-    alert(nextD_btn);
+
+// disembarking_btn.addEventListener("click",function(){
+   
+        
     
-});
+   
+// })
 
-nextD_btn.addEventListener("click",function(){
-    alert("test");
-})
+// boarding_btn.addEventListener("click",function(){
+//     bus["Langa"] = 20;
+//     console.log(bus["Langa"])
+//        alert( bus.Langa++ +" "+" "+bus.Langa)
+// })
+
+// add_btn.addEventListener("click",function(){
+//     alert()
+// })
+
+
+// nextD_btn.addEventListener("click",function(){
+//     alert()
+
+// })
