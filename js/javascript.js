957f3a363362e41186cc5ba21a1ba5f7daca532d
v203 <vuyisamr203@gmail.com>
2021-06-23 12:32:51 +0200

    gh

---
+++ js/javascript.js
@@ -0,0 +1,22 @@
+var disembarking_btn = document.querySelector(".getoff");
+var boarding_btn = document.querySelector(".geton")
+var add_btn = document.querySelector(".add_btn")
+var nextD_btn =  document.querySelector(".nextd_btn")
+
+
+disembarking_btn.addEventListener("cilck",function(){
+alert("test get off");
+})
+
+boarding_btn.addEventListener("cilck",function(){
+    alert("test get on");
+})
+
+add_btn.addEventListener("click",function(){
+    alert(nextD_btn);
+    
+});
+
+nextD_btn.addEventListener("click",function(){
+    alert("test");
+})
